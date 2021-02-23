757a6e5085c211e1e0dcd5e55dbc336b080bdc18
weeSnail <56113177+Anes-Marzooki@users.noreply.github.com>
2021-02-23 09:10:05 +0100

    Update warmUp4.js

--- warmUp4.js
+++ warmUp4.js
@@ -1,57 +1,6 @@
-/**
- * 1 - Complete the function cube that returns the cube of x:
- * @param {number} x
- * @return {number}
- */
-function cube(x) {
-    // your code here
+// 1-Write a function that takes a string as an input and returns the reverse of each letter 
+//followed by a number starting from zero(solve it using while loop).
+// example:    reverseStr('hello'); "o1l2l3e4h"
 
-}
-
-/**
- * 2 - Write a function sameLength that accepts two strings as arguments, and returns true if those strings have the same length, and false otherwise.
- * @param {string} string1
- * @param {string} string2
- * @return{boolean}
- */
-function sameLength(string1, string2) {
-
-}
-
-/**
- * 3 - Write a function called scoreToGrade that accepts a number as a parameter and returns a string representing a letter grade corresponding to that score.
- * @param {number} score
- * @return {string}
- *
- */
-function scoreToGrade(score){
-
-}
-
-
-/**
- * USE RECURSION
- * Repeating a String n Times: Let's write a function called repeatString that takes two parameters: a string str, which is the string to be repeated, and count -- a number representing how many times the string str should be repeated
- * repeatString('dog', 0); // => ''
- * repeatString('dog', 1); // => 'dog'
- * repeatString('dog', 2); // => 'dog' + 'dog' => 'dogdog'
- * repeatString('dog', 3); // => 'dog' + 'dog' + 'dog' => 'dogdogdog'
- *
- * @param {string} str
- * @param {number} count
- * @return {string}
- */
-function repeatString(str, count) {
-    // TODO: your code here
-}
-
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//ignore the below code // //
-module.exports.cube = cube; //
-module.exports.sameLength = sameLength; //
-module.exports.scoreToGrade = scoreToGrade; //
-module.exports.repeatString = repeatString; //
-// // // // // // // // // // // // // // //
-
+// 2-write a function that takes array of strings and returns an array of the strings that have the same length
+// example :  ['hi','hello','welcome','hy'] == > ["hi","hy"]
