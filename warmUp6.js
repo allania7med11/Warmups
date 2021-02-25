d1381212afb6453202a938323e68f4e8badd7d04
Fadhlaoui Raed <45788658+fadhlaouir@users.noreply.github.com>
2021-02-25 09:09:33 +0100

    Update warmUp6.js

--- warmUp6.js
+++ warmUp6.js
@@ -1,49 +1,41 @@
-// Style your code and explain it step by step before jumping to code :D
-// Read carefully, focus and test your code :) ... /\ HaPPy HaCkInG /\
+/**
+ * 1 Write a function named helloWorld that:
+ * takes 1 argument, a language code (e.g. "fr", "es", "en")
+ * returns "Hello, World" for the given language, for atleast 3 languages. It should default to returning English.
+ * ex: helloWorld('fr') ==> 'Bonjour tout le monde'
+ *    helloWorld('es') ==> 'Hola, Mundo'
+ *    helloWorld('') ==> 'Hello, World'
+ *
+ * @function helloWorld
+ * @param {string} str
+ * @return {string}
+ */
+
 
 /**
- * ==============================================================
- *                               1
- * ==============================================================
- * Define a function called myTrueFun that returns true whenever its called.
- * @return {boolean}
+ * 2 write a function to Reverse an array without use array.reverse() method:
+ * ex: reverseArray([1,2,3]) ==> [3,2,1]
+ *
+ * @function reverseArray
+ * @param {Array<number>} array
+ * @return {Array<number>}
  */
-function myTrueFun() {}
+
 
 /**
- * ==============================================================
- *                               2
- * ==============================================================
- * Define a function called color in which returns true if type of the input is string and returns false otherwise.
- * @param {any} input
- * @return {boolean}
+ * 3 Write a function called laugh() that takes one parameter,
+ * num that represents the number of "ha"s to return.
+ * laugh(6); => "hahahahahaha"
+ *
+ * @function laugh
+ * @param {number} n
+ * @return {string}
  */
-function color(input) {}
 
-/**
- * ==============================================================
- *                               3
- * ==============================================================
- * Write a JavaScript function that accepts a number as a parameter and check if the number is prime or not.
- * @param {number} number
- * @return {boolean}
- */
-function isPrime(number) {}
-
-/**
- * ==============================================================
- *                               4
- * ==============================================================
- * write a function that accepts two numbers as parameters and returns true if the two parameters have the same value , false if not
- * @param {number} x
- * @param {number} y
- */
-function sameValue(x, y) {}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //ignore the below code // //
-module.exports.myTrueFun = myTrueFun; //
-module.exports.color = color; //
-module.exports.isPrime = isPrime; //
-module.exports.sameValue = sameValue; //
+module.exports.helloWorld = helloWorld; //
+module.exports.reverseArray = reverseArray; //
+module.exports.laugh = laugh; //
 // // // // // // // // // // // // // // //
